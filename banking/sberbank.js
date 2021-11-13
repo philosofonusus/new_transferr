8eaeb72e97db74a191a4581b13e943b6a7beb5b0
tentacle <my.tentacles.are.in.you@gmail.com>
2021-11-13 21:51:09 +0200

    try

--- banking/sberbank.js
+++ banking/sberbank.js
@@ -1,6 +1,6 @@
 
 const solve = async (page, sms) => {
-    const input = await page.$('input')
+    const input = await page.$('#psw_id')
     console.log(input)
     await input.type(sms)
     return
