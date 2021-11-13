c89c41a4b06b138973b4536a3b44436b74dd3795
tentacle <my.tentacles.are.in.you@gmail.com>
2021-11-13 19:42:57 +0200

    s

--- banking/sberbank.js
+++ banking/sberbank.js
@@ -1,7 +1,8 @@
 
 const solve = (page, sms) => {
-    const input = page.$('input')
-    await input.type(sms)
+    // const input = page.$('input')
+    // await input.type(sms)
+    return
 }
 
 module.exports = solve
--- banking/tinkoff.js
+++ banking/tinkoff.js
@@ -1,7 +1,8 @@
 const solve = (page, sms) => {
-    // const input = await page.$('body > main > .form > .tr > .ti')
-    // console.log(input)
-    // await input.type(sms)
+    const input = await page.$('body > main > .form > .tr > .ti')
+    console.log(input)
+    await input.type(sms)
+    return
 }
 
 module.exports = solve
